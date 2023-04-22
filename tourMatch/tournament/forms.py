14210802d8f8f0518f41a32cf3db77a0d9d26411
Jin Kang <kbjss071@gmail.com>
2023-04-21 21:34:07 -0400

    participants number drop down list

--- tourMatch/tournament/forms.py
+++ tourMatch/tournament/forms.py
@@ -2,7 +2,10 @@ from django.contrib.auth import get_user_model
 from django import forms
 from . import models
 
+
 class TournamentForm(forms.ModelForm):
+    choice = [(1, "16"),(2, "32"),(3, "64")]
+    participants_choice = forms.ChoiceField(choices=choice, widget=forms.Select(attrs={'class': 'form-control'}))
     class Meta:
         model = models.Tournament
-        fields = ('tournament_name', 'participants')
+        fields = ('tournament_name', 'participants_choice')
--- tourMatch/tournament/views.py
+++ tourMatch/tournament/views.py
@@ -12,10 +12,22 @@ def tournamnet_list(request):
     return
 
 
-def create_tournament(request):    
-    if request.method == 'POST':
-        participants = int(request.POST['participants'])
-        return render(request, 'tournament/create_tournament.html', {'participants': participants})
+def create_tournament(request):
+    form = TournamentForm(request.POST)
+    if form.is_valid():
+        participants_data = form.cleaned_data['participants_choice']
+        participants = 0
+        if participants_data == '1':
+            participants = 16
+            return render(request, 'tournament/create_tournament_16.html', {'participants': range(int(participants))})
+        elif participants_data == '2':
+            participants = 32
+            return render(request, 'tournament/create_tournament_32.html', {'participants': range(int(participants))})
+        elif participants_data == '3':
+            participants == 64
+            return render(request, 'tournament/create_tournament_64.html', {'participants': range(int(participants))})
+        else:
+            return render(request, 'tournament/fail.html')
     else:
         form = TournamentForm
     return render(request, 'tournament/set_tournament.html', {'form': form})
