e4d8816bbf5a69ab124fc3dcf1e1d95320888ed0
Jin Kang <kbjss071@gmail.com>
2023-04-23 23:10:59 -0400

    first attempt to create tournament page

--- tourMatch/tournament/views.py
+++ tourMatch/tournament/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib.auth.decorators import login_required
 from .forms import TournamentForm
+import random
 
 # Create your views here.
 @login_required
@@ -18,17 +19,22 @@ def create_tournament(request):
         participants_data = form.cleaned_data['participants_choice']
         participants = 0
         if participants_data == '1':
-            participants = 16
-            return render(request, 'tournament/create_tournament_16.html', {'participants': range(int(participants))})
+            participants = randomize(list(range(1, 17)))
+            return render(request, 'tournament/create_tournament_16.html', {'participants': participants})
         elif participants_data == '2':
-            participants = 32
-            return render(request, 'tournament/create_tournament_32.html', {'participants': range(int(participants))})
+            participants = randomize(list(range(1, 33)))
+            return render(request, 'tournament/create_tournament_32.html', {'participants': participants})
         elif participants_data == '3':
-            participants == 64
-            return render(request, 'tournament/create_tournament_64.html', {'participants': range(int(participants))})
+            participants == randomize(list(range(1, 65)))
+            return render(request, 'tournament/create_tournament_64.html', {'participants': participants})
         else:
             return render(request, 'tournament/fail.html')
     else:
         form = TournamentForm
     return render(request, 'tournament/set_tournament.html', {'form': form})
 
+def randomize(list):
+    randomized = []
+    while(len(list)>0):
+        randomized.append(list.pop(random.randint(0, len(list)-1)))
+    return randomized
