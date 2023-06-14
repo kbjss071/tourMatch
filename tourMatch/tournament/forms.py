7238be812b339cf8fd030123c01bcb3196a47dc8
Jin Kang <kbjss071@gmail.com>
2023-06-14 17:01:49 -0400

    changed style for creating tournament

--- tourMatch/tournament/forms.py
+++ tourMatch/tournament/forms.py
@@ -5,7 +5,7 @@ from . import models
 
 class TournamentForm(forms.ModelForm):
     choice = [(1, "16"),(2, "32"),(3, "64")]
-    participants_choice = forms.ChoiceField(choices=choice, widget=forms.Select(attrs={'class': 'form-control'}))
+    participants_choice = forms.ChoiceField(choices=choice, widget=forms.Select(attrs={'class': 'form-control mt-2', 'style': 'width: 60px;'}))
     random = forms.BooleanField(label="Randomize Players", required=False, widget=forms.CheckboxInput(attrs={'class': 'largerCheck'}))
     class Meta:
         model = models.Tournament
